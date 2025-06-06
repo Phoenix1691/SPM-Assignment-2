da200f66df3ad047c27b1b0983de6b99c8a90c15
Phoenix1691 <134489290+Phoenix1691@users.noreply.github.com>
2025-06-07 03:34:58 +0800

    change to python

--- SPM Assignment 2/Program.cs
+++
@@ -1,3 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("This is a simple C# program to demonstrate the use of Console.WriteLine method.");
